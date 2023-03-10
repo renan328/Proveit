39ee87b4f8bb69845c80475a2ac3c26cac1c4324
renan328 <renanrodri38@gmail.com>
2023-03-10 19:12:22 -0300

    Scroll categoria completo + aprimoramento visual

--- frontend/App.js
+++ frontend/App.js
@@ -2,19 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Pages/Home';
-import { Raleway_100Thin, Raleway_500Medium, Raleway_700Bold, useFonts } from '@expo-google-fonts/raleway';
+import { Raleway_100Thin, Raleway_200ExtraLight, Raleway_300Light, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, Raleway_800ExtraBold, Raleway_900Black, useFonts } from '@expo-google-fonts/raleway';
 
 export default function App() {
 
-  const [fontsLoaded] = useFonts({ Raleway_100Thin, Raleway_500Medium, Raleway_700Bold });
+  const [fontsLoaded] = useFonts({ Raleway_100Thin, Raleway_200ExtraLight, Raleway_300Light, Raleway_400Regular, Raleway_500Medium, Raleway_600SemiBold, Raleway_700Bold, Raleway_800ExtraBold, Raleway_900Black, });
 
-  if (!fontsLoaded) {
-    return 
-      <AppLoading/>
-    
-    
-  }
-  else{
-    <Home/>
-  }
+  return (
+
+    //View principal do app, define resolução, etc.
+    <View style={{
+      overflow: 'hidden',
+      width: '100%',
+    }}>
+
+      <StatusBar translucent backgroundColor="transparent" />
+      <Home />
+    </View>
+  );
 }
