7da37fca94e03b21de287e68bb728af4ae1e454b
romagrinho <112034207+romagrinho@users.noreply.github.com>
2023-03-20 23:45:50 +0000

    Atualização tela cadastro de usuario

--- frontend/src/Pages/Cadastro_de_usuario/index.jsx
+++ frontend/src/Pages/Cadastro_de_usuario/index.jsx
@@ -4,23 +4,40 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { TouchableOpacity } from 'react-native-web';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
-import { SelectList } from 'react-native-dropdown-select-list'
+import { MultipleSelectList, SelectList } from 'react-native-dropdown-select-list'
 
 export default function Cadastro_de_usuario({ navigation }) {
-    
+
     const [selected, setSelected] = React.useState('');
- 
+
     const data = [
-        {key:'1', value:'teste'},
-        {key:'2', value:'teste'},
-        {key:'3', value:'teste'},
-        {key:'4', value:'teste'}
+        { key: '1', value: 'Aves' },
+        { key: '2', value: 'Bebidas' },
+        { key: '3', value: 'Bolos' },
+        { key: '4', value: 'Carnes' },
+        { key: '5', value: 'Doces' },
+        { key: '6', value: 'Frutos do Mar' },
+        { key: '7', value: 'Japones' },
+        { key: '8', value: 'Lanches' },
+        { key: '9', value: 'LowCarb' },
+        { key: '10', value: 'Massa' },
+        { key: '11', value: 'Molhos' },
+        { key: '12', value: 'Rapidas' },
+        { key: '13', value: 'Saladas' },
+        { key: '14', value: 'Salgados' },
+        { key: '15', value: 'Sanduiches' },
+        { key: '16', value: 'Snacks' },
+        { key: '17', value: 'Sobremesas' },
+        { key: '18', value: 'Sopas' },
+        { key: '19', value: 'Torta' },
+        { key: '20', value: 'Vegano' },
+        { key: '21', value: 'Vegetariano' },
     ];
 
     return (
 
         <View style={styles.container}>
-            
+
             {/* Header */}
             <LinearGradient colors={['#FF7152', '#FFB649']} style={[styles.header, styles.shadowProp]}>
                 <View style={styles.cadastro_de_usuario}>
@@ -32,7 +49,7 @@ export default function Cadastro_de_usuario({ navigation }) {
                     source={require('../../assets/usuario96.png')}
                 />
             </LinearGradient>
-            
+
             {/* Imagem de perfil */}
             <View style={styles.cadastro}>
                 <Text style={styles.suafoto}>Sua foto</Text>
@@ -42,47 +59,55 @@ export default function Cadastro_de_usuario({ navigation }) {
                 />
             </View>
 
-            {/* Formulário */}  
+            {/* Formulário */}
             <View style={styles.inputs}>
-                <Text style={styles.digite_nome}>Nome</Text>
-                <TextInput style={styles.nomeInput}
-                    placeholder='Digite seu nome'></TextInput>
-                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#fff' }} />
-                <Text style={styles.digite_nomeusuario}>Nome de usuário</Text>
-                <TextInput style={styles.nomeusuarioInput}
-                    placeholder='Digite seu nome de usuário'></TextInput>
-                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#fff' }} />
-                <Text style={styles.digite_email}>E-mail</Text>
-                <TextInput style={styles.emailInput}
-                    placeholder='Digite seu e-mail'></TextInput>
-                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#fff' }} />
-                <Text style={styles.digite_senha}>Senha</Text>
-                <TextInput style={styles.senhaInput}
-                    placeholder='Digite sua senha'></TextInput>
-                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#fff' }} />
-                <TextInput style={styles.redigitesenhaInput}
-                    placeholder='Redigite sua senha'></TextInput>
-                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#fff' }} />
+                <View style={styles.inputNome}>
+                    <Text style={styles.digite_nome}>Nome</Text>
+                    <TextInput style={styles.nomeInput} placeholder='Digite seu nome'></TextInput>
+                </View>
+
+                <View  style={styles.inputNomeUsuario}>
+                    <Text style={styles.digite_nomeusuario}>Nome de usuário</Text>
+                    <TextInput style={styles.nomeusuarioInput} placeholder='Digite seu nome de usuário'></TextInput>
+                </View>
+                                  
+                <View style={styles.inputEmail}>
+                    <Text style={styles.digite_email}>E-mail</Text>
+                    <TextInput style={styles.emailInput} placeholder='Digite seu e-mail'></TextInput>
+                </View>
+                                
+                <View style={styles.InputSenha}>
+                    <Text style={styles.digite_senha}>Senha</Text>
+                    <TextInput style={styles.senhaInput}  placeholder='Digite sua senha'></TextInput>
+                    <TextInput style={styles.redigitesenhaInput} placeholder='Redigite sua senha'></TextInput>
+                </View>
+                               
+                <View style={styles.InputCategoria}>
                 <Text style={styles.digite_favcategoria}>Suas categorias favoritas</Text>
-                <SelectList style={styles.favcategoriaInput}
-                    data={data} 
-                    setSelected= {setSelected}
+                    <MultipleSelectList style={styles.favcategoriaInput} data={data}
+                    setSelected={setSelected}
                     placeholder='Alguma categoria'
-                    searchPlaceholder = 'Adicionar  '
+                    searchPlaceholder='Adicionar'
+                    notFoundText='Categoria não encontrada'
+                    fontFamily='Raleway_600SemiBold'
                     boxStyles={styles.favcategoriaInput}
                     inputStyles={styles.favcategoriafonteInput}
                     dropdownStyles={styles.favcategorialistaInput}
-                    dropdownTextStyles={styles.favcategoriafonteInput}
-                />           
+                    dropdownTextStyles={styles.favcategoriafonteInput}>
+                    
+                    </MultipleSelectList>
+                </View>
+            </View>   
+
+             {/* Botão */}
+            <View style={styles.botoes}>
+                <TouchableOpacity onPress={() => navigation.navigate('')} >
+                    <LinearGradient colors={['#FF7152', '#FFB649']} start={{ x: -1, y: 1 }}
+                        end={{ x: 2, y: 1 }} style={styles.button} >
+                        <Text style={styles.buttonText}>Pronto</Text>
+                    </LinearGradient>
+                </TouchableOpacity>
             </View>
-                    <View style={styles.botoes}>
-                        <TouchableOpacity onPress={() => navigation.navigate('')} >
-                            <LinearGradient colors={['#FF7152', '#FFB649']} start={{ x: -1, y: 1 }}
-                                end={{ x: 2, y: 1 }} style={styles.button} >
-                                <Text style={styles.buttonText}>Pronto</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </View>
         </View>
     )
 }
@@ -91,8 +116,16 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
+    },
+
+    inputs: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
 
     },
+
     header: {
         height: 103,
         width: '100%',
@@ -111,6 +144,7 @@ const styles = StyleSheet.create({
 
 
     },
+
 
     cadastro_de_usuario: {
         top: -8
@@ -157,7 +191,6 @@ const styles = StyleSheet.create({
         marginHorizontal: '47px',
         paddingHorizontal: '7px',
         fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
         color: '#505050',
         height: '50px',
         width: '296px',
@@ -182,11 +215,14 @@ const styles = StyleSheet.create({
         marginLeft: 47,
     },
 
+    inputNomeUsuario:{
+        marginTop:10
+    },
+
     nomeusuarioInput: {
         marginHorizontal: '47px',
         paddingHorizontal: '7px',
         fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
         color: '#505050',
         height: '50px',
         width: '296px',
@@ -211,11 +247,14 @@ const styles = StyleSheet.create({
         marginLeft: 47,
     },
 
+    inputEmail:{
+        marginTop:10
+    },
+
     emailInput: {
         marginHorizontal: '47px',
         paddingHorizontal: '7px',
         fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
         color: '#505050',
         height: '50px',
         width: '296px',
@@ -240,11 +279,14 @@ const styles = StyleSheet.create({
         marginLeft: 47,
     },
 
+    InputSenha:{
+        marginTop:10
+    },
+
     senhaInput: {
         marginHorizontal: '47px',
         paddingHorizontal: '7px',
         fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
         color: '#505050',
         height: '50px',
         width: '296px',
@@ -273,7 +315,6 @@ const styles = StyleSheet.create({
         marginHorizontal: '47px',
         paddingHorizontal: '7px',
         fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
         color: '#505050',
         height: '50px',
         width: '296px',
@@ -292,47 +333,49 @@ const styles = StyleSheet.create({
         marginTop: 10
     },
 
+    InputCategoria:{
+        marginTop:10
+    },
+
     digite_favcategoria: {
         display: 'flex',
-        marginLeft: 47,
     },
 
     favcategoriaInput: {
-        marginHorizontal: '47px',
-        marginTop: 10,    
+        width: 296,
+        marginTop: 10,
         shadowOffset: {
             width: 0,
             height: 2,
         },
         shadowOpacity: 0.25,
-        shadowRadius: 4,                      
-    },
-   
-    favcategoriafonteInput:{
-        fontSize: '11px',
-        fontFamily: 'Raleway_600SemiBold',
-        color: '#505050',   
-        marginTop: 5,    
+        shadowRadius: 4,
     },
 
-    favcategorialistaInput:{        
-        marginHorizontal: '47px',      
+    favcategoriafonteInput: {
+        fontSize: '11px',
+        color: '#505050',
+        marginTop: 5,
+    },
+
+    favcategorialistaInput: {
+        width: 296,
         shadowOffset: {
             width: 0,
             height: 2,
         },
         shadowOpacity: 0.25,
-        shadowRadius: 4,        
-    },   
+        shadowRadius: 4,
+    },
 
     button: {
         width: 200,
-        marginTop:30,
+        marginTop: 30,
         backgroundColor: 'orange',
         color: '#FFF',
         borderRadius: 5,
         padding: 10,
-        alignSelf: 'center',       
+        alignSelf: 'center',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -342,7 +385,7 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
     },
 
-    buttonText: {      
+    buttonText: {
         textAlign: 'center',
         color: '#FFF',
         fontFamily: 'Raleway_700Bold',
