7fc79b4cc087eb5587b5303750c3e6f171fa4dbe
romagrinho <112034207+romagrinho@users.noreply.github.com>
2023-04-04 01:12:06 +0000

    atualizando tela forgot_password

--- frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
+++ frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
@@ -1,33 +1,89 @@
 import { StyleSheet } from 'react-native';
 
+
+
+
 const styles = StyleSheet.create({
 
-    imageContainer: {
-        display: 'flex',
-        height: '20%',
-    },
-    
-    main: {
-        backgroundColor: '#fff',
-        width: '100%',
-        height: '80%',
-        display: 'flex',
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        marginTop: '135px',
-        borderTopLeftRadius: 120,
-        borderTopRightRadius: 120,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 7,
-        top: "-70"
-    },
-   
+
+
+
+    imageContainer: {
+
+        display: 'flex',
+
+        height: '20%',
+
+    },
+
+   
+
+    main: {
+
+        backgroundColor: '#fff',
+
+        width: '100%',
+
+        height: '100%',
+
+        display: 'flex',
+
+        textAlign: 'center',
+
+        alignItems: 'center',
+
+        justifyContent: 'flex-start',
+
+        marginTop: '100',
+
+        borderTopRightRadius: 120,
+
+        shadowColor: "#000",
+
+        shadowOffset: {
+
+            width: 0,
+
+            height: 0,
+
+        },
+
+        shadowOpacity: 0.25,
+
+        shadowRadius: 7,
+
+        /* top: "-70" */
+
+    },
+
+
+
+
+    circuloIcon: {
+
+        width: 150,
+
+        height: 150,
+
+        borderRadius:'70%' ,
+
+        backgroundColor: '#FF7152',
+
+      },
+
+   
+
+      IconCad: {
+
+        display: 'flex',
+
+        alignItems: 'center',
+
+      },
+
 });
+
+
+
 
 export default styles;
--- frontend/src/Pages/EsqueciMinhaSenha/index.jsx
+++ frontend/src/Pages/EsqueciMinhaSenha/index.jsx
@@ -1,23 +1,64 @@
 import React, { Component, useState } from 'react';
-import { View, Text, StyleSheet, ImageBackground, Image, TextInput, StatusBar, ScrollView } from 'react-native';
+
+import { View, SafeAreaView, Text, StyleSheet, ImageBackground, Image, TextInput, StatusBar, ScrollView, Dimensions } from 'react-native';
+
 import { LinearGradient } from 'expo-linear-gradient';
+
 import { TouchableOpacity } from 'react-native-web';
+
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+
 import { MultipleSelectList, SelectList } from 'react-native-dropdown-select-list';
+
 import validator from 'validator';
+
 import styles from './esqueciminhasenha.module';
 
-export default function EsqueciMinhaSenha() {
-    return (
-        <View style={styles.container}>
 
-            <View style={styles.imageContainer}>
-                <ImageBackground source={require('../../assets/headerBG.jpg')}/>
-            </View>
-            <View style={styles.main}>
-        
-          
-            </View>
-        </View>
-    );
+
+
+const screenHeight = Dimensions.get('window').height;
+
+
+
+
+export default function EsqueciMinhaSenha() {
+
+    return (
+
+        <View style={styles.container}>
+
+
+
+
+            <View style={styles.imageContainer}>
+
+                <ImageBackground source={require('../../assets/headerBG.jpg')} style={{ height: screenHeight * 0.6 }}> </ImageBackground>
+
+            </View>
+
+            <View style={styles.main}>
+
+                <View style={styles.circuloIcon}>
+
+                    <View style={styles.IconCad}>
+
+                        <Image
+
+                            style={{ width: '89px', height: '86px' }}
+
+                            source={require('../../assets/IconCad.png')}
+
+                        />
+
+                    </View>
+
+                </View>
+
+            </View>
+
+        </View>
+
+    );
+
 }
