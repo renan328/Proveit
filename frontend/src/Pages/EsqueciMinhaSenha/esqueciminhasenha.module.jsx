96af8bf891aff34bbb8155ae050735cd0f73bf09
romagrinho <112034207+romagrinho@users.noreply.github.com>
2023-04-05 01:25:38 +0000

    Atualizando tela forgotmypassword

--- frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
+++ frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
@@ -1,89 +1,48 @@
 import { StyleSheet } from 'react-native';
 
-
-
-
 const styles = StyleSheet.create({
 
+    imageContainer: {
+        display: 'flex',
+        height: '20%',
+    },
 
+    main: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: '100',
+        borderTopRightRadius: 120,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 7,
+        /* top: "-70" */
+    },
 
+    circuloIcon: {
+        display: 'flex',
+        alignItems:'center',
+        flexDirection:"column",
+        justifyContent: 'center',
+        width: 150,
+        height: 150,
+        borderRadius: '70%',
+        backgroundColor: '#FF7152',
+    },
 
-    imageContainer: {
-
-        display: 'flex',
-
-        height: '20%',
-
-    },
-
-   
-
-    main: {
-
-        backgroundColor: '#fff',
-
-        width: '100%',
-
-        height: '100%',
-
-        display: 'flex',
-
-        textAlign: 'center',
-
-        alignItems: 'center',
-
-        justifyContent: 'flex-start',
-
-        marginTop: '100',
-
-        borderTopRightRadius: 120,
-
-        shadowColor: "#000",
-
-        shadowOffset: {
-
-            width: 0,
-
-            height: 0,
-
-        },
-
-        shadowOpacity: 0.25,
-
-        shadowRadius: 7,
-
-        /* top: "-70" */
-
-    },
-
-
-
-
-    circuloIcon: {
-
-        width: 150,
-
-        height: 150,
-
-        borderRadius:'70%' ,
-
-        backgroundColor: '#FF7152',
-
-      },
-
-   
-
-      IconCad: {
-
-        display: 'flex',
-
-        alignItems: 'center',
-
-      },
-
+    Qualeoseu:{
+        color: '#505050',
+        fontFamily: 'Raleway_800ExtraBold',
+        fontSize: 36,
+    },
 });
-
-
-
 
 export default styles;
