43ba5584b1913460cc5cb83702faf4f5ebf4bed1
romagrinho <romariosaguilar@gmail.com>
2023-04-05 15:52:20 -0300

    Atualizando tela forgotmypassword

--- frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
+++ frontend/src/Pages/EsqueciMinhaSenha/esqueciminhasenha.module.jsx
@@ -4,17 +4,19 @@ const styles = StyleSheet.create({
 
     imageContainer: {
         display: 'flex',
-        height: '20%',
+        height: '10%',
     },
 
     main: {
+        display: 'flex',
         backgroundColor: '#fff',
         width: '100%',
-        height: '100%',
+        height: '63%',
         display: 'flex',
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
+       /*  textAlign: 'center',
+        alignItems: 'flex-center',
+        justifyContent: 'flex-start', */
+        justifyContent:'space-between',
         marginTop: '100',
         borderTopRightRadius: 120,
         shadowColor: "#000",
@@ -29,19 +31,94 @@ const styles = StyleSheet.create({
 
     circuloIcon: {
         display: 'flex',
+        alignContent:'space-around',
         alignItems:'center',
         flexDirection:"column",
         justifyContent: 'center',
+        alignSelf: 'center',
         width: 150,
         height: 150,
         borderRadius: '70%',
         backgroundColor: '#FF7152',
     },
+    
 
     Qualeoseu:{
         color: '#505050',
         fontFamily: 'Raleway_800ExtraBold',
-        fontSize: 36,
+        fontSize: 40,
+    },
+
+    Email:{
+        color: '#FF7152',
+        fontFamily: 'Raleway_800ExtraBold',
+        fontSize: 35,
+    },
+
+    Desc:{
+        color: '#505050',
+        fontFamily: 'Raleway_600SemiBold',
+        fontSize: 14,
+        opacity: '80%',
+    },
+
+    inputSingle: {
+        width: '80%',
+        display: 'flex',
+        /* justifyContent: 'space-around', */
+        /* marginTop: '20px', */
+    },  
+
+    defaultInput: {
+        paddingHorizontal: '7px',
+        fontSize: '13px',
+        color: '#505050',
+        height: '50px',
+        width: '100%',
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        /* marginTop: 10, */
+        fontFamily: 'Raleway_600SemiBold',
+    },
+
+    button: {
+        display: 'flex',
+        /* marginBottom: 49,
+        marginTop: 49, */
+        width: 275,
+        backgroundColor: 'orange',
+        color: '#FFF',
+        borderRadius: 5,
+        padding: 8,
+        alignSelf: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+    },
+
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFF',
+        fontFamily: 'Raleway_700Bold',
+    },
+
+    WaveImage:{
+
     },
 });
 
--- frontend/src/Pages/EsqueciMinhaSenha/index.jsx
+++ frontend/src/Pages/EsqueciMinhaSenha/index.jsx
@@ -14,9 +14,11 @@ export default function EsqueciMinhaSenha() {
     return (
 
         <View style={styles.container}>
+            {/* Imagem fundo */}
             <View style={styles.imageContainer}>
                 <ImageBackground source={require('../../assets/headerBG.jpg')} style={{ height: screenHeight * 0.6 }}> </ImageBackground>
             </View>
+            {/* Container de componentes */}
             <View style={styles.main}>
                 <View style={styles.circuloIcon}>
                     <View style={styles.IconCad}>
@@ -26,9 +28,33 @@ export default function EsqueciMinhaSenha() {
                         />
                     </View>
                 </View>
-                <View style={styles.TextEmail}>
+                <View style={styles.Texts}>
                     <Text style={styles.Qualeoseu}>Qual é o seu</Text>
                     <Text style={styles.Email}>Email?</Text>
+                    <Text style={styles.Desc}>Digite o email cadastrado para que enviemos um código de autenticação.</Text>
+                </View>
+
+                {/* Input */}
+                <View style={styles.inputSingle}>
+                    <TextInput style={styles.defaultInput} placeholder='E-mail'></TextInput>
+                </View>
+
+                {/* Botão */}
+                <View style={styles.botoes}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Main')} >
+                        <LinearGradient colors={['#FF7152', '#FFB649']} start={{ x: -1, y: 1 }}
+                            end={{ x: 2, y: 1 }} style={styles.button} >
+                            <Text style={styles.buttonText}>Pronto</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+                    {/* Wave Image */}
+                    <View style={styles.WaveImage}>
+                        <Image
+                             style={{ width: '376px', height: '235px' }}
+                            source={require('../../assets/element_graphics_wave_full.png')}
+                        />
+                    </View>
                 </View>
             </View>
         </View>
